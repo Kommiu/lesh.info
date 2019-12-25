3d938de0989995fe121b3a30195a90beddaa3455
kommiu <nefiltrolf@gmail.com>
2019-12-25 12:51:03 +0300

    Added tables, put bot functionality into new class

--- bot.py
+++ bot.py
@@ -18,9 +18,6 @@ from telegram.ext import Updater, MessageHandler, Filters
 import dialogflow_v2 as df
 from processors import processor_map, process_default 
 
-
-
-
 def send_typing_action(func):
     """Sends typing action while processing func command."""
     @wraps(func)
@@ -31,83 +28,130 @@ def send_typing_action(func):
     return command_func
 
 
+class Bot():
+    def __init__(self, project_id, language_code, tg_token, admins, db_path):
+        self.project_id = project_id
+        self.language_code = language_code
+        self.session_client = df.SessionsClient()
+        self.updater = Updater(
+            token=tg_token,
+            use_context=True,
+        )
+        self.admins = [int(x) for x in admins.split(' ')]
+        self.dispatcher = updater.dispatcher
+        self.db_path = db_path
+        self.processor_map = {
+            'who_comes': self._process_whocomes,
+        }
 
-@send_typing_action
-def _respond(
-        update,
-        context,
-        session_client=None,
-        project_id=None,
-        language_code=None,
-):
-    if not update.message or not update.message.text:
-        return
+    def run(self):
+        
+        respond_handler = MessageHandler(Filters.text, self._respond)
+        self.dispatcher.add_handler(respond_handler)
+        self.dispatcher.add_error_handler(self._error_handler)
+        updater.start_polling()
 
-    df_session_id = context.user_data.get('df_session_id', None)
-    if df_session_id is None:
-        df_session_id = update.effective_user.id
+    def process(self, response, update, context, session):
+        processor = self.processor_map.get(
+            response.query_result.intent.display_name,
+            self._process_default
+        )
+        processor(response, update, context, session)
 
-    df_session = session_client.session_path(project_id, df_session_id)
-    text_input = df.types.TextInput(
-        text=update.message.text,
-        language_code=language_code,
-    )
-    query_input = df.types.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-        session=df_session,
-        query_input=query_input,
-    )
+    def _process_default(self, response, update, context, session):
+        response_lines = response.query_result.fulfillment_text.split('\n')
+        for line in response_lines:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=line,
+            )
 
-    processor = processor_map.get(
-        response.query_result.intent.display_name,
-        process_default
-    )
-    processor(response, update, context)
+    @send_typing_action
+    def _respond(
+            self,
+            update,
+            context,
+    ):
+        if not update.message or not update.message.text:
+            return
+
+        df_session_id = context.user_data.get('df_session_id', None)
+        if df_session_id is None:
+            df_session_id = update.effective_user.id
+
+        df_session = self.session_client.session_path(self.project_id, df_session_id)
+        text = update.message.text
+        response = self.detect_intent(text, df_session)
+
+        processor = self.processor_map.get(
+            response.query_result.intent.display_name,
+            process_default
+        )
+        processor(response, update, context, db_session)
+        
+
+    def _error_handler(self, update, context):
+        devs = self.admins
+        if update.effective_message:
+            text = "Hey. I'm sorry to inform you that an error happened while I tried to handle your update. " \
+                   "My developer(s) will be notified."
+            update.effective_message.reply_text(text)
+
+        trace = "".join(traceback.format_tb(sys.exc_info()[2]))
+        payload = ""
+        if update.effective_user:
+            payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
+        if update.effective_chat:
+            payload += f' within the chat <i>{update.effective_chat.title}</i>'
+            if update.effective_chat.username:
+                payload += f' (@{update.effective_chat.username})'
+        if update.poll:
+            payload += f' with the poll id {update.poll.id}.'
+        text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
+               f"</code>"
+        for dev_id in devs:
+            context.bot.send_message(dev_id, text, parse_mode=ParseMode.HTML)
+        raise
+    
+
+    def _process_whocomes(self, response, update, context, session):
+        name = response.query_result.parameters['teacher_name']
+        with sqlite3.connect(self.db_path) as conn:
+            c = conn.cursor()
+            c.execute(
+                '''
+                SELECT Фамилия, Имя 
+                FROM teachers
+                WHERE Фамилия=?
+                ''',
+                (name, )
+            )
+            rows = c.fetchall()
+        if rows is None:
+            new_response = self.detect_intent('No', session)
+        else:
+            new_response = self.detect_intent('Yes', session)
+
+        self.process(new_response, update, context, session)
 
 
+    def detect_intent(self, text, session):
+        text_input = df.types.TextInput(text=text, language_code=self.language_code)
+        query_input = df.types.QueryInput(text=text_input)
+        response = self.session_client.detect_intent(session=session, query_inpute=query_input)
+        return response
 
-def error_handler(update, context, devs):
-    if update.effective_message:
-        text = "Hey. I'm sorry to inform you that an error happened while I tried to handle your update. " \
-               "My developer(s) will be notified."
-        update.effective_message.reply_text(text)
-
-    trace = "".join(traceback.format_tb(sys.exc_info()[2]))
-    payload = ""
-    if update.effective_user:
-        payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
-    if update.effective_chat:
-        payload += f' within the chat <i>{update.effective_chat.title}</i>'
-        if update.effective_chat.username:
-            payload += f' (@{update.effective_chat.username})'
-    if update.poll:
-        payload += f' with the poll id {update.poll.id}.'
-    text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
-           f"</code>"
-    for dev_id in devs:
-        context.bot.send_message(dev_id, text, parse_mode=ParseMode.HTML)
-    raise
 
 @click.command()
 @click.argument('tg_token', envvar='TG_TOKEN')
 @click.argument('project_id', envvar='PROJECT_ID')
 @click.argument('admins', envvar='ADMINS')
 @click.option('--language_code', default='ru')
-def run(tg_token, project_id, admins, language_code):
+@click.option('--db_path', default='/tmp/local.db')
+def run(tg_token, project_id, admins, language_code, db_path):
     session_client = df.SessionsClient()
-
-    updater = Updater(
-        token=tg_token,
-        use_context=True,
-    )
-    dispatcher = updater.dispatcher
-    respond = partial(_respond, project_id=project_id, session_client=session_client, language_code=language_code)
-    respond_handler = MessageHandler(Filters.text, respond)
-    dispatcher.add_handler(respond_handler)
+    bot = Bot(project_id=project_id, language_code=language_code, tg_token=tg_token, admins=admins, db_path=db_path)
+    bot.run()
     
-    admins = [int(x) for x in admins.split(' ')]
-    dispatcher.add_error_handler(partial(error_handler, devs=admins))
-    updater.start_polling()
-
 if __name__ == '__main__':
     run()
